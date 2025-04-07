14aee68fdcc8e16dcb6da7d115218589a06b57fb
mousearthouse <mousearthousee@gmail.com>
2025-04-07 21:13:25 +0300

    🎱 Still making profile page

--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -31,14 +31,15 @@ const ProfilePage = () => {
                 <div className='user-data-block'>
                     <div className='avatar-container'>
                     {userData.avatar && (
-                            <img src={getImageUrl()} alt="Аватар" />
-                        )}
+                        <img src={getImageUrl()} alt="Аватар" />
+                    )}
                     </div>
                     <PersonalData userData={userData} />
                     <Contacts userData={userData} />
                 </div>
                 <div className='education-info-block'>
-                    <Education userData={userData} />
+                    <h2>{userData.lastName} {userData.firstName} {userData.patronymic}</h2>
+                    <Education />
                 </div>
             </div>
         </main>
@@ -48,36 +49,38 @@ const ProfilePage = () => {
 const PersonalData = ({ userData }: { userData: UserProfileDto }) => {
     return (
         <div className="block">
-            <h3 className="block-title">Личные данные</h3>
+            <div className='block-row'>
+                <h3>Личные данные</h3>
+            </div>
             <div className="block-content">
                 {userData.contacts && (
                     <div className="block-row">
                         <span className="block-label">Пол</span>
                         <span className="block-value">{userData.gender}</span>
-                        <hr />
                     </div>
                 )}
+                <hr />
                 {userData.birthDate && (
                     <div className="block-row">
                         <span className="block-label">Дата рождения</span>
                         <span className="block-value">{userData.birthDate}</span>
-                        <hr />
                     </div>
                 )}
+                <hr />
                 {userData.citizenship && (
                     <div className="block-row">
                         <span className="block-label">Гражданство</span>
                         <span className="block-value">{userData.citizenship.name}</span>
-                        <hr />
                     </div>
                 )}
+                <hr />
                 {userData.email && (
                     <div className="block-row">
                         <span className="block-label">Email</span>
                         <span className="block-value">{userData.email}</span>
-                        <hr />
                     </div>
                 )}
+                <hr />
             </div>
         </div>
     );
@@ -87,36 +90,37 @@ const Contacts = ({ userData }: { userData: UserProfileDto }) => {
 
     return (
         <div className="block">
-            <h3 className="block-title">Контакты</h3>
+            <div className='block-row'>
+                <h3>Контакты</h3>
+            </div>
             <div className="block-content">
                 {userData.contacts && (
                     <div className="block-row">
                         <span className="block-label">Телефон</span>
                         <span className="block-value">{}</span>
-                        <hr />
                     </div>
                 )}
+                <hr />
                 {userData.email && (
                     <div className="block-row">
                         <span className="block-label">Дополнительный E-mail:</span>
                         <span className="block-value">{userData.email}</span>
-                        <hr />
                     </div>
                 )}
-        
+                <hr />
                 {userData.address && (
                     <div className="block-row">
                         <span className="block-label">Адрес</span>
                         <span className="block-value">{userData.address}</span>
-                        <hr />
                     </div>
                 )}
+                <hr />
             </div>
         </div>
     );
 };
 
-const Education = ({ userData }: { userData: UserProfileDto }) => {
+const Education = () => {
     const [studentData, setStudentData] = useState<StudentDto>({} as StudentDto);
     useEffect(() => {
         const fetchProfile = async () => {
@@ -134,7 +138,64 @@ const Education = ({ userData }: { userData: UserProfileDto }) => {
     return (
         <div className="block">
             <h3 className="block-title">Образование</h3>
-            <h3 className='block-title'></h3>
+
+            {(studentData.educationEntries ?? []).map((educationEntry, id) => (
+                <div key={id}>
+                    <div className="container-row">
+                        <div className='block-education-row1'>
+                            <span className="block-label">Года обучения</span>
+                            <span className="block-value">{educationEntry.educationYears.name}</span>
+                        </div>
+                        <div className='block-education-row2'>
+                            <span className="block-label">Номер зачетной книги</span>
+                            <span className="block-value">{educationEntry.creditBooknumber}</span>
+                        </div>
+                    </div>
+                    <hr />
+                    <div className="container-row">
+                        <div className='block-education-row1'>
+                            <span className="block-label">Форма обучения</span>
+                            <span className="block-value">{educationEntry.educationForm.name}</span>
+                        </div>
+                        <div className='block-education-row2'>
+                            <span className="block-label">База</span>
+                            <span className="block-value">{educationEntry.educationBase.name}</span>
+                        </div>
+                    </div>
+                    <hr />
+                    <div className="container-row">
+                        <div className='block-education-row1'>
+                            <span className="block-label">Факультет</span>
+                            <span className="block-value">{educationEntry.faculty.name}</span>
+                        </div>
+                    </div>
+                    <hr />
+                    <div className="container-row">
+                        <div className='block-education-row1'>
+                            <span className="block-label">Направление</span>
+                            <span className="block-value">{educationEntry.educationDirection.name}</span>
+                        </div>
+                    </div>
+                    <hr />
+                    <div className="container-row">
+                        <div>
+                            <span className="block-label">Профиль</span>
+                            <span className="block-value">{educationEntry.educationProfile.name}</span>
+                        </div>
+                    </div>
+                    <hr />
+                    <div className="container-row">
+                        <div className='block-education-row1'>
+                            <span className="block-label">Курс</span>
+                            <span className="block-value">{educationEntry.course}</span>
+                        </div>
+                        <div className='block-education-row2'>
+                            <span className="block-label">Группа</span>
+                            <span className="block-value">{educationEntry.group.name}</span>
+                        </div>
+                    </div>
+                </div>
+            ))}
             
             <div className="block-content">
                 
